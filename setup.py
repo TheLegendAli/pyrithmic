1b79362cb8be561f80bc77d770464a41793b62b5
Ali Mousavi <amousavi14@gmail.com>
2024-09-24 01:58:33 +0000

    changed setup

--- setup.py
+++ setup.py
@@ -31,15 +31,12 @@ setuptools.setup(
         'Topic :: Software Development :: Build Tools',
 
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3 :: Only',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.11',
     install_requires=[
         'numpy',
         'pandas',
